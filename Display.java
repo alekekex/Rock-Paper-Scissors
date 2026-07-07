3c6ccbb1ffee4c7b322563781fc6c0476842258d
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 14:48:50 +0800

    added ascii objects display

--- Display.java
+++ Display.java
@@ -1,3 +1,34 @@
 public class Display {
+    public static void displayRock() {
+        System.out.println("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """);
+    }
 
+    public static void displayPaper() {
+        System.out.println("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """);
+    }
+
+    public static void displayScissors() {
+        System.out.println("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """);
+    }
 }
