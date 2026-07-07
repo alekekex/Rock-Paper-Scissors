93826c7259bc3c333c14217ee6cd072b04a84969
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 16:48:49 +0800

    added score attribute

--- Player.java
+++ Player.java
@@ -1,11 +1,13 @@
 public class Player {
     private String name;
     private boolean isAI;
+    private int score;
     private Stats stats;
 
     public Player(String name, boolean isAI) {
         this.name = name;
         this.isAI = isAI;
+        this.score = 0;
         this.stats = new Stats();
     }
 
@@ -17,7 +19,15 @@ public class Player {
         return this.isAI;
     }
 
+    public int getScore() {
+        return this.score;
+    }
+
     public Stats getStats() {
         return this.stats;
+    }
+
+    public void addScore() {
+        this.score++;
     }
 }
