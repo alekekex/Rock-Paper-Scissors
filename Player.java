33580ad752f031ec4f497a99d391d2c11da983cb
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 17:29:24 +0800

    removed score attribute

--- Player.java
+++ Player.java
@@ -6,7 +6,7 @@ public class Player {
     public Player(String name, boolean isAI) {
         this.name = name;
         this.isAI = isAI;
-        this.stats = new Stats();//test
+        this.stats = new Stats();
     }
 
     public String getName() {
