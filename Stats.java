79ed9a5c520284307ee26949b6e52110fff1037e
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 17:52:14 +0800

    test push

--- Stats.java
+++ Stats.java
@@ -35,7 +35,7 @@ public class Stats {
         this.currStreak++;
 
         if(this.currStreak == 2)
-            this.winStreaks++;
+            this.winStreaks++;//TEST
     }
 
     public void addLosses() {
