80b92dfa37fc1f014047f521ff9b293d2d07ea0e
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 14:55:51 +0800

    added constructors and getters

--- Player.java
+++ Player.java
@@ -1,3 +1,23 @@
 public class Player {
+    private String name;
+    private boolean isAI;
+    private Stats stats;
 
+    public Player(String name, boolean isAI) {
+        this.name = name;
+        this.isAI = isAI;
+        this.stats = new Stats();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean isAI() {
+        return this.isAI;
+    }
+
+    public Stats getStats() {
+        return this.stats;
+    }
 }
