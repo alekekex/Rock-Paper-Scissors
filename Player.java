3ec030bbb2544700813d53472d19d1008eb2a502
alekekex <alexander_khan@dlsu.edu.ph>
2026-07-07 17:27:17 +0800

    removed score

--- Player.java
+++ Player.java
@@ -1,13 +1,11 @@
 public class Player {
     private String name;
     private boolean isAI;
-    private int score;
     private Stats stats;
 
     public Player(String name, boolean isAI) {
         this.name = name;
         this.isAI = isAI;
-        this.score = 0;
         this.stats = new Stats();
     }
 
@@ -19,15 +17,7 @@ public class Player {
         return this.isAI;
     }
 
-    public int getScore() {
-        return this.score;
-    }
-
     public Stats getStats() {
         return this.stats;
-    }
-
-    public void addScore() {
-        this.score++;
     }
 }
